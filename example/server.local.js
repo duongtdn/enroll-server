91c4f570fa0686445d46894640d96fad80f55dbc
duongtdn <duongtdn@gmail.com>
2018-08-20 12:33:09 +0000

    first commt

---
+++ example/server.local.js
@@ -0,0 +1,14 @@
+"use strict"
+
+require('dotenv').config()
+
+const db = require('database-test-helper')
+const enrolldb = require('enrolldb-test-helper')
+
+db.start().add({enrolldb}).init(() => {
+  // const app = require('./app.local')
+  const PORT = process.env.PORT_LOCAL_ENROLL || 3200;
+  // const httpServer = require('http').createServer(app);
+  // httpServer.listen(PORT)
+  console.log(`\n# ENROLL-SERVICES is running at http://localhost:${PORT}\n`);
+});
